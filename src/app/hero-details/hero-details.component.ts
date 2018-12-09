664b9fcdf1ccab8a22f32f7ad78d10bc3fb05361
Zachary Graham <zakgraham@gmail.com>
2018-12-09 13:46:39 -0700

    add Input to hero-details-component

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-hero-details',
+  selector: 'app-hero-detail',
   templateUrl: './hero-details.component.html',
   styleUrls: ['./hero-details.component.css']
 })
 export class HeroDetailsComponent implements OnInit {
+  @Input() hero: Hero;
 
   constructor() { }
 
