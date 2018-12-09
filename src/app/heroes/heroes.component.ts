ad03fe4b8007307a2f27cfcae4e5f046ce7b27c5
Zachary Graham <zakgraham@gmail.com>
2018-12-09 14:06:30 -0700

    finish hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { HEROES } from './../moch-heroes';
+import { HeroService } from '../hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -9,19 +9,23 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
 }
-
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-
 
 }
